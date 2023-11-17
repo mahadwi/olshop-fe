1912a7998089467e5b6ba67519eb7acaba0fb826
fadlifirdaus <fadli.firdaus@dansmultipro.com>
2023-11-17 16:31:32 +0700

    update

--- src/containers/login/indexLogin.js
+++ src/containers/login/indexLogin.js
@@ -2,13 +2,71 @@ import React, { Component } from 'react'
 import LoginIndex from '../../components/login/loginIndex'
 import IndexNavbar from '../../components/navbar/IndexNavbar'
 import IndexFooter from '../../components/footer/indexFooter'
+import axios from 'axios'
+import { GetProduct, GetBrand, GetCategory, GetBanner } from '../../config/api';
 
 
 export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product : [],
+      brands : [],
+      categories : [],
+      headerBanner : "",
+      filterBrand :'',
+    }
+  }
+
+    async componentDidMount() {
+       try {
+        const id = window.location.href.split('/')[4]
+        const response = await axios.get(GetProduct, {params:
+          {category_id:[id],
+          brand_id:this.state.filterBrand
+          }
+        })
+        {
+            let produk = [];
+            const datas = response.data.data
+            console.log('datas:',datas)
+            datas.map((data)=>{
+              produk.push(data)
+            })
+            this.setState({product:produk})
+            this.handleDropDownDesign()
+            this.handleDropDownCollective()
+            this.handleGetBanner()
+        }
+       } catch (error) {
+            console.log('error :',error)
+       }
+      }
+
+      async handleDropDownDesign() {
+        try {
+         const response = await axios.get(GetBrand)
+           this.setState({brands: response.data.data})
+        } catch (error) {
+           console.log(error)
+        }
+       }
+
+       async handleDropDownCollective(){
+        try {
+          const response = await axios.get(GetCategory)
+          this.setState({categories: response.data.data})
+        } catch (error) {
+          console.log(error) 
+        }
+       }
   render() {
     return (
         <>
-       <IndexNavbar/> 
+       <IndexNavbar
+       brands={this.state.brands}
+       categories={this.state.categories}
+       /> 
       <LoginIndex/>
       <br/>
       <br/>
--- src/containers/register/indexRegister.js
+++ src/containers/register/indexRegister.js
@@ -2,12 +2,70 @@ import React, { Component } from 'react'
 import RegisterIndex from '../../components/register/registerIndex'
 import IndexNavbar from '../../components/navbar/IndexNavbar'
 import IndexFooter from '../../components/footer/indexFooter'
+import axios from 'axios'
+import { GetProduct, GetBrand, GetCategory, GetBanner } from '../../config/api';
 
 export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product : [],
+      brands : [],
+      categories : [],
+      headerBanner : "",
+      filterBrand :'',
+    }
+  }
+
+    async componentDidMount() {
+       try {
+        const id = window.location.href.split('/')[4]
+        const response = await axios.get(GetProduct, {params:
+          {category_id:[id],
+          brand_id:this.state.filterBrand
+          }
+        })
+        {
+            let produk = [];
+            const datas = response.data.data
+            console.log('datas:',datas)
+            datas.map((data)=>{
+              produk.push(data)
+            })
+            this.setState({product:produk})
+            this.handleDropDownDesign()
+            this.handleDropDownCollective()
+            this.handleGetBanner()
+        }
+       } catch (error) {
+            console.log('error :',error)
+       }
+      }
+
+      async handleDropDownDesign() {
+        try {
+         const response = await axios.get(GetBrand)
+           this.setState({brands: response.data.data})
+        } catch (error) {
+           console.log(error)
+        }
+       }
+
+       async handleDropDownCollective(){
+        try {
+          const response = await axios.get(GetCategory)
+          this.setState({categories: response.data.data})
+        } catch (error) {
+          console.log(error) 
+        }
+       }
   render() {
     return (
       <div>
-        <IndexNavbar/>
+        <IndexNavbar
+          brands={this.state.brands}
+          categories={this.state.categories}
+        />
       <RegisterIndex/>
       <br/>
       <br/>
