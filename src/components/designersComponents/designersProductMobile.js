23f295d408c7f6e52aa0147b9e557401011506dd
fadlifirdaus <fadli.firdaus@dansmultipro.com>
2023-12-01 18:46:42 +0700

    feat : fixing defect

--- src/components/designersComponents/designersProductMobile.js
+++ src/components/designersComponents/designersProductMobile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardBody, CardGroup, CardTitle, Col, Row } from 'react-bootstrap';
+import { IconStarFilled, IconHeart, IconHeartFilled } from '@tabler/icons-react';
+import './designers-product.scooped.scss'
 
 export default class CollectiveProductMobile extends Component {
   handleDescProd = (description) => {
@@ -8,37 +10,38 @@ export default class CollectiveProductMobile extends Component {
 
   render() {
     const { products } = this.props;
+    const windowWidth = this.props.windowWidth
     const formatter = new Intl.NumberFormat('id-ID', {
       style: 'currency',
       currency: 'IDR',
     });
 
     return (
-      <div>
-        <Row>
-          {products.map((data, index) => (
-            // <Col key={index} style={{ marginBottom: '1rem', flex:'50%' }}>
-              <Card key={index} style={{ border: 'none', width: '100%', flex:'50%'}}>
-                <a style={{ cursor: 'pointer' }} onClick={() => this.handleDescProd(data.description)}>
-                  <CardBody>
-                    <Card.Img style={{ width: '158px', height: '201px' }} src={data.images[0] || 'no image'} alt={data.name} />
-                    <CardTitle style={{ width: '158px', height: '201px', fontSize: '12 px', fontFamily: 'Cambay', marginTop: '10px' }}>
-                      <b>{data.name}</b>
-                      <p></p>
-                      <p style={{ color: '#FFAC33' }}>
-                        <b>{formatter.format(data.sale_price)}</b>
-                      </p>
-                      <p>
-                        <b>5.0</b> (10) Terjual
-                      </p>
-                    </CardTitle>
-                  </CardBody>
-                </a>
-              </Card>
-            // </Col>
-          ))}
-        </Row>
-      </div>
+      <div> {windowWidth > 900 ? (
+        <CardGroup style={{marginLeft:'10%', marginRight:'5%'}}>
+    {products.map((data,index)=>{
+      return(
+        <div style={{marginBottom:'9rem', marginRight:'3rem'}}>
+            <Card style={{border:'none',width:'194px', height:'178px'}} key={index}>
+                <CardBody>
+                <div className='overlay'>
+                <img className='' style={{width:'194px', height:'178px'}} src={data.images}/>
+                  <div className='icon-overlay'><IconHeart/></div>
+                </div>
+                <CardTitle style={{fontSize:'12px',fontFamily:'Cambay'}}>
+                <br/>
+                <b>{data.name}</b>
+                <p></p>
+                <p style={{color:'#FFAC33'}}><b>{formatter.format(data.sale_price)}</b></p>
+                <p><b>5.0</b> <IconStarFilled style={{ color: '#FFAC33' }} size={12} /> (10) Terjual</p>
+                </CardTitle>
+                </CardBody>
+            </Card>
+            </div>
+    )})}
+    </CardGroup>
+    ):null}
+  </div>
     );
   }
 }
