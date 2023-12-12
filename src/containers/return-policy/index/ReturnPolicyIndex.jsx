d4179401a1a811c6425bd621749b05c33cbb4dad
MyFRA <tomyntapss@gmail.com>
2023-12-12 10:44:03 +0700

    feat: dynamic return police

--- src/containers/return-policy/index/ReturnPolicyIndex.jsx
+++ src/containers/return-policy/index/ReturnPolicyIndex.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import BreadCrumbComponent from "../../../components/general/breadcrumb/BreadCrumbComponent";
 import ContainerComponent from "../../../components/general/container/ContainerComponent";
-import ReturnPolicyIllustration from './../../../images/return-policy/ReturnPolicyIllustration.png'
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { IconBrandWhatsapp } from "@tabler/icons-react";
 import './return-policy.scoped.scss'
+import Api from "../../../utils/Api";
 
 export default function ReturnPolicyIndex() {
 
@@ -19,15 +19,26 @@ export default function ReturnPolicyIndex() {
      * 
      */
     const [breadcrumb, setBreadcrumb] = useState([])
+    const [returnPoliceObject, setReturnPoliceObject] = useState({})
 
     useEffect(() => {
         loadBreadcrumb()
+        loadReturnPoliceObject()
     }, [])
 
     // Automatically scrolls to top whenever pathname changes
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
+
+    const loadReturnPoliceObject = () => {
+        Api.get('/return-police')
+            .then((res) => {
+                if (res) {
+                    setReturnPoliceObject(res.data.data[0])
+                }
+            })
+    }
 
     const loadBreadcrumb = () => {
         setBreadcrumb([
@@ -49,37 +60,16 @@ export default function ReturnPolicyIndex() {
                 <div className="return-policy-wrapper">
                     <div className="left">
                         <div className="inner">
-                            <img src={ReturnPolicyIllustration} alt="" />
+                            <img src={returnPoliceObject.image_url} alt="" />
                         </div>
                     </div>
                     <div className="right">
                         <div className="inner">
                             <div className="box">
-                                <p>
-                                    All orders are final, we do not accept refund/returns for :
-
-                                    <ul>
-                                        <li>Discounted/Sale items</li>
-                                        <li>tems shipped with international shipping</li>
-                                        <li>Items that have been received for more than 24 hours</li>
-                                    </ul>
-
-                                    Items that are not according to your liking/taste
-                                    We can only accept returns for items that do not match the description or conditions written on the product description page.
-                                    Delivery will be borne by customer.
-
-
-                                </p>
-
-                                <p>
-                                    <b>IMPORTANT</b> : Submit your Return Request with an unboxing video within 24 hours after you receive your order
-                                </p>
-
-                                <p>
-                                    You can always consign the non-returnable items at www.luxi.com. Contact our Client Advisors for more information.
+                                <p dangerouslySetInnerHTML={{ __html: returnPoliceObject.description }}>
                                 </p>
                             </div>
-                            <Link className="btn-action"><IconBrandWhatsapp /> <span>Contact Us</span></Link>
+                            <a target="_blank" href={`https://wa.me/${returnPoliceObject.cp}`} className="btn-action"><IconBrandWhatsapp /> <span>Contact Us</span></a>
                         </div>
                     </div>
                 </div>
