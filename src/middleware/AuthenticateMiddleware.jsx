e14a855fcf5f1d30f511bde32858097b5e47d875
MyFRA <tomyntapss@gmail.com>
2023-12-13 10:38:44 +0700

    fix: auth

--- src/middleware/AuthenticateMiddleware.jsx
+++ src/middleware/AuthenticateMiddleware.jsx
@@ -1,17 +1,32 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 import { AuthUserContext } from '../context/AuthUserContext'
+import { LoadingContext } from '../context/LoadingContext'
 
 export default function AuthenticateMiddleware() {
     const { user } = useContext(AuthUserContext)
+    const { setLoading } = useContext(LoadingContext)
+
+    useEffect(() => {
+        if (user === false) {
+            setLoading(true)
+        } else {
+            setLoading(false)
+        }
+    }, [user])
 
     return (
         <>
             {
-                user === null ?
-                    <Navigate to={'/login'} />
-                    :
-                    <Outlet />
+                user !== false ?
+                    <>
+                        {
+                            user === null ?
+                                <Navigate to={'/login'} />
+                                :
+                                <Outlet />
+                        }
+                    </> : <></>
             }
         </>
     )
