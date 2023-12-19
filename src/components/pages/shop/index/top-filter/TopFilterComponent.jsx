c2c42758b4c6ceaec5071c3522311b905ded69ef
MyFRA <tomyntapss@gmail.com>
2023-12-19 20:56:11 +1300

    feat: relevance mobile

--- src/components/pages/shop/index/top-filter/TopFilterComponent.jsx
+++ src/components/pages/shop/index/top-filter/TopFilterComponent.jsx
@@ -1,9 +1,11 @@
 import './top-filter.scoped.scss'
 import './top-filter.css'
-import { IconAdjustmentsHorizontal } from '@tabler/icons-react';
+import { IconAdjustmentsHorizontal, IconChevronDown, IconX } from '@tabler/icons-react';
 import Select from 'react-select';
+import { useState } from 'react';
 
 export default function TopFilterComponent({ productResultAmount, sortOptions, selectedSortOption, setSelectedSortOption, setShowMobileFilter }) {
+    const [showMobileRelevance, setShowMobileRelevance] = useState(false);
 
     const handleChange = (selectedOption) => {
         setSelectedSortOption(selectedOption)
@@ -11,6 +13,11 @@ export default function TopFilterComponent({ productResultAmount, sortOptions, s
 
     const toggleMobileFilter = () => {
         setShowMobileFilter((current) => !current);
+      
+    };
+
+    const toggleMobileRelevence = () => {
+        setShowMobileRelevance((current) => !current);
     };
 
     return (
@@ -28,45 +35,57 @@ export default function TopFilterComponent({ productResultAmount, sortOptions, s
             </div>
             <div className='right'>
                 <div className='inner-right'>
-                    <Select
-                        styles={{
-                            control: (baseStyles, state) => ({
-                                ...baseStyles,
-                                borderWidth: '0px',
-                                boxShadow: 'none',
-                                backgroundColor: state.isDisabled ? 'transparent' : 'transparent',
-                            }),
-                            container: (baseStyles, state) => ({
-                                ...baseStyles,
-                                width: '100%',
-                            }),
-                            input: (baseStyles, state) => ({
-                                ...baseStyles,
-                                color: '#545454',
-                                fontSize: '12px',
-                                fontWeight: '300',
-                                fontFamily: "'Inter', sans-serif"
-                            }),
-                            option: (baseStyles, state) => ({
-                                ...baseStyles,
-                                backgroundColor: state.isDisabled ? 'transparent' : 'transparent',
-                                color: '#000',
-                                fontSize: '12px',
-                                fontWeight: state.isDisabled ? '700' : '400',
-                                fontFamily: "'Inter', sans-serif",
-                                borderBottom: state.isDisabled ? '1px solid #C4C4C4;' : '0px',
-                                "&:hover": {
-                                    backgroundColor: state.isDisabled ? '#FFF' : "#000",
-                                    color: state.isDisabled ? '#000' : '#FFF'
-                                }
-                            }),
+                    <div className='relevance-desktop'>
+                        <Select
+                            styles={{
+                                control: (baseStyles, state) => ({
+                                    ...baseStyles,
+                                    borderWidth: '0px',
+                                    boxShadow: 'none',
+                                    backgroundColor: state.isDisabled ? 'transparent' : 'transparent',
+                                }),
+                                container: (baseStyles, state) => ({
+                                    ...baseStyles,
+                                    width: '100%',
+                                }),
+                                input: (baseStyles, state) => ({
+                                    ...baseStyles,
+                                    color: '#545454',
+                                    fontSize: '12px',
+                                    fontWeight: '300',
+                                    fontFamily: "'Inter', sans-serif"
+                                }),
+                                option: (baseStyles, state) => ({
+                                    ...baseStyles,
+                                    backgroundColor: state.isDisabled ? 'transparent' : 'transparent',
+                                    color: '#000',
+                                    fontSize: '12px',
+                                    fontWeight: state.isDisabled ? '700' : '400',
+                                    fontFamily: "'Inter', sans-serif",
+                                    borderBottom: state.isDisabled ? '1px solid #C4C4C4;' : '0px',
+                                    "&:hover": {
+                                        backgroundColor: state.isDisabled ? '#FFF' : "#000",
+                                        color: state.isDisabled ? '#000' : '#FFF'
+                                    }
+                                }),
 
-                        }}
-                        value={selectedSortOption}
-                        onChange={handleChange}
-                        options={sortOptions}
-                    />
+                            }}
+                            value={selectedSortOption}
+                            onChange={handleChange}
+                            options={sortOptions}
+                        />
+                    </div>
+                    <div className='relevance-mobile'>
+                        <button className='relevance-button' onClick={toggleMobileRelevence}>{selectedSortOption.label.substring(0,15)} &nbsp; <IconChevronDown /></button>
+                    </div>
                 </div>
+            </div>
+            <div className={`relevance-filter ${showMobileRelevance ? 'mobile-show': 'mobile-hide'}`}>
+                <div className='relevance-filter-close'><button onClick={() => setShowMobileRelevance(false)}><IconX size={32} /></button></div>
+                <div className='relevance-filter-title'>Relevance</div>
+                <ul>
+                    {sortOptions.map(({value, label}) => <li><button onClick={() => {setSelectedSortOption({value, label});setShowMobileRelevance(false)}}>{label}</button></li>)}
+                </ul>
             </div>
         </div>
     )
