944bf4dfcde962d39515980738229d4fd5a0c2f9
MyFRA <tomyntapss@gmail.com>
2023-12-29 20:44:34 +1300

    feat: early api implementation for consignment page

--- src/containers/consignment/index/ConsignmentIndex.jsx
+++ src/containers/consignment/index/ConsignmentIndex.jsx
@@ -5,6 +5,7 @@ import { Link, useLocation } from "react-router-dom";
 import './consignment.scoped.scss'
 import Api from "../../../utils/Api";
 import { LoadingContext } from "../../../context/LoadingContext";
+import { LanguageContext } from "../../../context/LanguageContext";
 import { IconArrowRight, IconWallet, IconDiamond } from "@tabler/icons-react";
 
 const icons = {
@@ -13,14 +14,6 @@ const icons = {
 };
 
 const DATA_DUMMY = {
-    title0: 'SELL WITH US',
-    title1: 'Sell your pre-loved items to make some extra money or to declutter your closet',
-    aboutYoutubeVideoId: 'yqQfrJw8s0g',
-    aboutTitle0: 'About Consignment',
-    aboutTitle1: 'Our consignment option will get you the highest price in comparison to our other selling options.',
-    firstHeroImage: 'https://i.ibb.co/r7r1G0c/image.png',
-    firstHeroTitle0: 'Sell Your Luxury Items With Us In Just A Few Clicks',
-    firstHeroTitle1: 'No hassle, no fuss, just self-upload or send us your pre-loved items',
     brands: [
         {
             image: 'https://i.ibb.co/7WdTrK5/image.png',
@@ -110,6 +103,8 @@ export default function ConsignmentIndex() {
      * 
      */
     const { setLoading } = useContext(LoadingContext)
+    const { language } = useContext(LanguageContext)
+    const suffix = language == 'id' ? '' : '_en';
 
     /**
      * Main State
@@ -130,17 +125,14 @@ export default function ConsignmentIndex() {
 
     const loadConsignmentObject = () => {
         setLoading(true)
-        // TODO: connect API
-        // Api.get('/work-with-us')
-        //     .then((res) => {
-        //         if (res) {
-        //             setConsignmentObject(res.data.data[0])
-        //         }
-        //     }).finally(() => {
-        //         setLoading(false)
-        //     })
-        setConsignmentObject(DATA_DUMMY);
-        setLoading(false)
+        Api.get('/consignment')
+            .then((res) => {
+                if (res) {
+                    setConsignmentObject({...DATA_DUMMY,...res.data.data[0]})
+                }
+            }).finally(() => {
+                setLoading(false)
+            })
     }
 
     const loadBreadcrumb = () => {
@@ -162,16 +154,16 @@ export default function ConsignmentIndex() {
 
                 <div className="consignment-wrapper">
                     <div className='title'>
-                        <div className='title0'>{consignmentObject.title0}</div>
-                        <div className='title1'>{consignmentObject.title1}</div>
+                        <div className='title0'>{consignmentObject['title'+suffix]}</div>
+                        <div className='title1' dangerouslySetInnerHTML={{__html: consignmentObject['description'+suffix]}}></div>
                     </div>
                     <div className='consignment-first-hero'>
                         <div className='consignment-first-hero-image'>
-                            <img src={consignmentObject.firstHeroImage} alt='Hero' />
+                            <img src={consignmentObject.sections?.at(0).image} alt='Hero' />
                         </div>
                         <div className='consignment-first-hero-detail'>
-                            <div className='_title0'>{consignmentObject.firstHeroTitle0}</div>
-                            <div className='_title1'>{consignmentObject.firstHeroTitle1}</div>
+                            <div className='_title0'>{consignmentObject.sections?.at(0)['title'+suffix] ?? ''}</div>
+                            <div className='_title1' dangerouslySetInnerHTML={{ __html: consignmentObject.sections?.at(0)['description'+suffix]?? ''}}></div>
                             <div className='_buttons'>
                                 <Link className='_button'>CONSIGN WITH US</Link>
                                 <button className='_button' onClick={() => {document.getElementById('consignment-about').scrollIntoView()}}>LEARN MORE</button>
@@ -181,11 +173,11 @@ export default function ConsignmentIndex() {
                     <div className='consignment-div' style={{margin: "4rem 0"}} />
                     <div className='consignment-about' id='consignment-about'>
                         <div className='consignment-about-video'>
-                            <iframe src={`https://www.youtube-nocookie.com/embed/${consignmentObject.aboutYoutubeVideoId}?si=A1ujlX7IktTEKpc2`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <iframe src={`https://www.youtube-nocookie.com/embed/${consignmentObject.sections?.at(1).link.split('/').at(-1)}?si=A1ujlX7IktTEKpc2`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         </div>
                         <div className='consignment-about-detail'>
-                            <div className='_title0'>{consignmentObject.aboutTitle0}</div>
-                            <div className='_title1'>{consignmentObject.aboutTitle1}</div>
+                            <div className='_title0'>{consignmentObject.sections?.at(1)['title'+suffix]}</div>
+                            <div className='_title1' dangerouslySetInnerHTML={{ __html: consignmentObject.sections?.at(1)['description'+suffix]}}></div>
                         </div>
                     </div>
                     <div className='consignment-div-title' style={{margin: "4rem 0"}}>BRANDS ACCEPTED</div>
