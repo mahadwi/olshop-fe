841b4ae3a270f85f4789f89d5d0fbde1e0a56f6d
MyFRA <tomyntapss@gmail.com>
2024-01-02 16:13:42 +1300

    refactor: use brand API

--- src/containers/consignment/index/ConsignmentIndex.jsx
+++ src/containers/consignment/index/ConsignmentIndex.jsx
@@ -6,89 +6,6 @@ import './consignment.scoped.scss'
 import Api from "../../../utils/Api";
 import { LoadingContext } from "../../../context/LoadingContext";
 import { LanguageContext } from "../../../context/LanguageContext";
-import { IconArrowRight, IconWallet, IconDiamond } from "@tabler/icons-react";
-
-const icons = {
-    'wallet': IconWallet,
-    'diamond': IconDiamond,
-};
-
-const DATA_DUMMY = {
-    brands: [
-        {
-            image: 'https://i.ibb.co/7WdTrK5/image.png',
-            name: 'Hermes',
-        },
-        {
-            image: 'https://i.ibb.co/xLJD0NT/image.png',
-            name: 'Channel',
-        },
-        {
-            image: 'https://i.ibb.co/10tY7tG/image.png',
-            name: 'Dior',
-        },
-        {
-            image: 'https://i.ibb.co/r7r1G0c/image.png',
-            name: 'Louis Vuitton',
-        },
-    ],
-    programs: [
-        {
-            image: 'https://i.ibb.co/QPZwfK3/image.png',
-            name: 'Belle&Kate ROOM Service',
-            step: [
-                {
-                    icon: 'wallet',
-                    name: 'UPLOAD SPECIFICATIONS ONLINE',
-                    description: 'Submit brief details and photo of your designer bag(s) (min 5 items) that you would like to consign with us. The team will conduct an initial review of the items virtually and will only pick up your bag(s) that have been accepted.',
-                },
-                {
-                    icon: 'wallet',
-                    name: 'UPLOAD SPECIFICATIONS ONLINE',
-                    description: 'Submit brief details and photo of your designer bag(s) (min 5 items) that you would like to consign with us. The team will conduct an initial review of the items virtually and will only pick up your bag(s) that have been accepted.',
-                },
-                {
-                    icon: 'wallet',
-                    name: 'UPLOAD SPECIFICATIONS ONLINE',
-                    description: 'Submit brief details and photo of your designer bag(s) (min 5 items) that you would like to consign with us. The team will conduct an initial review of the items virtually and will only pick up your bag(s) that have been accepted.',
-                },
-                {
-                    icon: 'wallet',
-                    name: 'UPLOAD SPECIFICATIONS ONLINE',
-                    description: 'Submit brief details and photo of your designer bag(s) (min 5 items) that you would like to consign with us. The team will conduct an initial review of the items virtually and will only pick up your bag(s) that have been accepted.',
-                },
-            ]
-        },
-        {
-            image: 'https://i.ibb.co/qsxMGWq/image.png',
-            name: 'Register your item yourself',
-            step: [
-                {
-                    icon: 'wallet',
-                    name: 'UPLOAD SPECIFICATIONS ONLINE',
-                    description: 'Submit brief details and photo of your designer bag(s) (min 5 items) that you would like to consign with us. The team will conduct an initial review of the items virtually and will only pick up your bag(s) that have been accepted.',
-                },
-                {
-                    icon: 'wallet',
-                    name: 'UPLOAD SPECIFICATIONS ONLINE',
-                    description: 'Submit brief details and photo of your designer bag(s) (min 5 items) that you would like to consign with us. The team will conduct an initial review of the items virtually and will only pick up your bag(s) that have been accepted.',
-                },
-                {
-                    icon: 'wallet',
-                    name: 'UPLOAD SPECIFICATIONS ONLINE',
-                    description: 'Submit brief details and photo of your designer bag(s) (min 5 items) that you would like to consign with us. The team will conduct an initial review of the items virtually and will only pick up your bag(s) that have been accepted.',
-                },
-                {
-                    icon: 'wallet',
-                    name: 'UPLOAD SPECIFICATIONS ONLINE',
-                    description: 'Submit brief details and photo of your designer bag(s) (min 5 items) that you would like to consign with us. The team will conduct an initial review of the items virtually and will only pick up your bag(s) that have been accepted.',
-                },
-            ]
-        }
-    ],
-    secondHeroTitle0: 'WANT TO PERSONALLY DROP OFF YOUR BAGS IN STORE?',
-    secondHeroTitle1: 'Ready to sell or trade your items? Bring accepted styles to Belle&Kate Room store and receive the most competitive price.',
-};
 
 export default function ConsignmentIndex() {
 
@@ -112,6 +29,8 @@ export default function ConsignmentIndex() {
      */
     const [breadcrumb, setBreadcrumb] = useState([])
     const [consignmentObject, setConsignmentObject] = useState({})
+    const [sections, setSections] = useState({})
+    const [brands, setBrands] = useState([])
 
     useEffect(() => {
         loadBreadcrumb()
@@ -125,14 +44,27 @@ export default function ConsignmentIndex() {
 
     const loadConsignmentObject = () => {
         setLoading(true)
-        Api.get('/consignment')
+        const getConsignment = Api.get('/consignment')
             .then((res) => {
                 if (res) {
-                    setConsignmentObject({...DATA_DUMMY,...res.data.data[0]})
+                    setConsignmentObject(res.data.data[0])
+                    setSections(res.data.data[0].sections.reduce((p, c) => {
+                        p[c.section] = c;
+                        return p;
+                    },{}));
                 }
-            }).finally(() => {
-                setLoading(false)
             })
+        const getBrands = Api.get('/brand')
+            .then((res) => {
+                if (res) {
+                    res.data.data.sort((a, b) => a.id - b.id)
+                    setBrands(res.data.data.slice(0,4));
+                }
+            })
+        Promise.all([getConsignment, getBrands])
+            .finally(() => {
+                setLoading(false);
+            });
     }
 
     const loadBreadcrumb = () => {
@@ -159,11 +91,11 @@ export default function ConsignmentIndex() {
                     </div>
                     <div className='consignment-first-hero'>
                         <div className='consignment-first-hero-image'>
-                            <img src={consignmentObject.sections?.at(0).image} alt='Hero' />
+                            <img src={sections[1]?.image} alt='Hero' />
                         </div>
                         <div className='consignment-first-hero-detail'>
-                            <div className='_title0'>{consignmentObject.sections?.at(0)['title'+suffix] ?? ''}</div>
-                            <div className='_title1' dangerouslySetInnerHTML={{ __html: consignmentObject.sections?.at(0)['description'+suffix]?? ''}}></div>
+                            <div className='_title0'>{sections[1]?.['title'+suffix] ?? ''}</div>
+                            <div className='_title1' dangerouslySetInnerHTML={{ __html: sections[1]?.['description'+suffix]?? ''}}></div>
                             <div className='_buttons'>
                                 <Link className='_button'>CONSIGN WITH US</Link>
                                 <button className='_button' onClick={() => {document.getElementById('consignment-about').scrollIntoView()}}>LEARN MORE</button>
@@ -182,26 +114,26 @@ export default function ConsignmentIndex() {
                     </div>
                     <div className='consignment-div-title' style={{margin: "4rem 0"}}>BRANDS ACCEPTED</div>
                     <div className='consignment-brands'>
-                        {consignmentObject.brands?.map(({image, name}) => {
+                        {brands?.map(({image_url: image, name}) => {
                             return <div className='consignment-brand'><img src={image} alt={name}/>{name}</div>
                         })}
                     </div>
                     <div className='consignment-div-title' style={{margin: "4rem 0"}}>HOW TO CONSIGN WITH US ?</div>
                     <div className='programs'>
-                        {consignmentObject.programs?.map(({image, name, step}, i) => {
+                        {[sections[4], sections[5]].map((c, i) => {
+                            if (c == undefined) return <></>;
                             return <>
                                 {i!=0?<div className='consignment-div' />:null}
                                 <div className='program-step'>
-                                    <img className='program-step-image' src={image} alt='brand' />
+                                    <img className='program-step-image' src={c.image} alt='brand' />
                                     <div className='program-step-content'>
-                                        <div className='program-step-title'>{name}</div>
+                                        <div className='program-step-title'>{c['title'+suffix]}</div>
                                         <div className='program-step-grid'>
-                                            {step.map(({icon, name, description}, i) => {
-                                                const Icon = icons[icon];
+                                            {c.cards.map((s, i) => {
                                                 return <div className='program-step-card'>
-                                                    <div className='program-step-card-icon'><Icon size={48} /></div>
-                                                    <div className='program-step-card-name'>{i+1}. {name}</div>
-                                                    <div className='program-step-card-description'>{description}</div>
+                                                    <div className='program-step-card-icon'><img src={s.icon} alt={'icon'} width={42}/></div>
+                                                    <div className='program-step-card-name'>{s['title'+suffix]}</div>
+                                                    <div className='program-step-card-description' dangerouslySetInnerHTML={{__html: s['description'+suffix]}}></div>
                                                 </div>
                                             })}
                                         </div>
@@ -213,8 +145,8 @@ export default function ConsignmentIndex() {
                 </div>
             </ContainerComponent>
             <div className='consignment-second-hero'>
-                <div className='_title0'>{consignmentObject.secondHeroTitle0}</div>
-                <div className='_title1'>{consignmentObject.secondHeroTitle1}</div>
+                <div className='_title0'>{sections[6]?.['title'+suffix]}</div>
+                <div className='_title1' dangerouslySetInnerHTML={{__html: sections[6]?.['description'+suffix]}}></div>
                 <Link className='_button'>BOOK APPOINTMENT</Link>
             </div>
         </div>
