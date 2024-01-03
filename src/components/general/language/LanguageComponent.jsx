d74e66ba7fe337e664f1fd881a2b26ae1eeea486
MyFRA <tomyntapss@gmail.com>
2024-01-03 17:37:32 +1300

    refacor: popup language

--- src/components/general/language/LanguageComponent.jsx
+++ src/components/general/language/LanguageComponent.jsx
@@ -23,31 +23,43 @@ export default function LanguageComponent({ loading }) {
     return (
         <div className={`language-component ${showLanguage ? 'show': ''}`}>
             <div className={`language-content ${showLanguage ? 'show': ''}`}>
+                <button className='close-modal' type='button' onClick={() => {
+                    setShowLanguage(false);
+                }}><IconX /></button>
+                <div className='title'>Looks like you’re shopping {!Intl.DateTimeFormat().resolvedOptions().locale.includes('id') ? 'not ' : null }from Indonesia</div>
                 <div className='languages'>
-                    <button className='close-modal' type='button' onClick={() => {
-                        setShowLanguage(false);
-                    }}><IconX /></button>
-                    <div className='language'>
-                        <img src='https://flagicons.lipis.dev/flags/4x3/us.svg' alt='US flag' />
-                        English
-                        <button onClick={() => {
-                            localStorage.setItem('popupLanguage', 'true')
-                            setShowLanguage(false);
-                            changeLanguage('en')
-                            setLanguage('en')
-                        }}>Choose Language</button>
-                    </div>
                     <div className='language'>
                         <img src='https://flagicons.lipis.dev/flags/4x3/id.svg' alt='Indonesia flag' />
-                        Indonesia
+                        <div className='list'>
+                            Indonesian
+                            <ul>
+                                <li>Prices and payment amounts are displayed in IDR</li>
+                            </ul>
+                        </div>
                         <button className='secondary' onClick={() => {
                             localStorage.setItem('popupLanguage', 'true')
                             setShowLanguage(false);
                             changeLanguage('id')
                             setLanguage('id')
-                        }}>Pilih Bahasa</button>
+                        }}>Select</button>
+                    </div>
+                    <div className='language'>
+                        <img src='https://flagicons.lipis.dev/flags/4x3/us.svg' alt='US flag' />
+                        <div className='list'>
+                            English (US)
+                            <ul>
+                                <li>Prices and payment amounts are displayed in $</li>
+                            </ul>
+                        </div>
+                        <button onClick={() => {
+                            localStorage.setItem('popupLanguage', 'true')
+                            setShowLanguage(false);
+                            changeLanguage('en')
+                            setLanguage('en')
+                        }}>Select</button>
                     </div>
                 </div>
+                <div className='bottom'><a href='/contact'>Contact us</a> if you have question about choosing language</div>
             </div>
         </div>
     )
