bc15aca6e56ddae78861969298206bb53e17acda
MyFRA <tomyntapss@gmail.com>
2024-01-18 16:10:51 +1300

    feat: product description detail and history

--- src/components/pages/shop/show/product-description-mobile/ProductDescriptionMobileComponent.jsx
+++ src/components/pages/shop/show/product-description-mobile/ProductDescriptionMobileComponent.jsx
@@ -1,23 +1,30 @@
+import { useContext } from 'react';
 import './product-description-mobile.scoped.scss'
+import { LanguageContext } from '../../../../../context/LanguageContext';
 import parse from 'html-react-parser';
+import StringUtil from '../../../../../utils/StringUtil'
 
 export default function ProductDescriptionMobileComponent({ productObj }) {
+    const { language } = useContext(LanguageContext)
+    const suffix = language == 'id' ? '' : '_en';
+
     return (
     <div className='product-description-body'>
         <div className='product-detail'>
             <h3 className='product-detail-title'>Product Detail</h3>
             <p className='product-description-text'>{parse(productObj.description ? productObj.description : '')}</p>
-            {/* <ul className='product-list-details'>
-                <li>roduct code: 1NE515_RDH0_F0F24</li>
-                <li>Zipper closure</li>
-                <li>Enameled metal triangle logo</li>
-                <li>Woven tape handle</li>
-                <li>Logo-print Re-Nylon lining</li>
-                <li>Height: 17cm</li>
-                <li>Width: 22cm</li>
-                <li>Length: 6cm</li>
-                <li>Imported</li>
-            </ul> */}
+            <ul className='product-list-details'>
+                <li><strong>Brand</strong>: {productObj.brand}</li>
+                <li><strong>Category</strong>: {productObj.category}</li>
+                <li><strong>Weight</strong>: {StringUtil.numberingWithDotFormat(Math.ceil(productObj.weight))} gr</li>
+                <li><strong>Height</strong>: {productObj.height}cm</li>
+                <li><strong>Width</strong>: {productObj.width}cm</li>
+                <li><strong>Length</strong>: {productObj.length}cm</li>
+                <li><strong>Color</strong>: {productObj.color}</li>
+                <li><strong>Condition</strong>: {productObj.condition}</li>
+            </ul>
+            <h3 className='product-detail-title'>History</h3>
+            <p className='product-description-text'>{parse(productObj['history'+suffix] ? productObj['history'+suffix] : '')}</p>
         </div>
         {/* <div className='product-materials'>
             <h3 className='product-material-title'>Materials</h3>
--- src/components/pages/shop/show/product-description/ProductDescriptionComponent.jsx
+++ src/components/pages/shop/show/product-description/ProductDescriptionComponent.jsx
@@ -3,12 +3,12 @@ import { IconStarFilled, IconHeart, IconHeartFilled, IconShare } from '@tabler/i
 import './product-description.scoped.scss'
 import parse from 'html-react-parser';
 import { LanguageContext } from '../../../../../context/LanguageContext';
+import StringUtil from '../../../../../utils/StringUtil'
 
 export default function ProductDescriptionComponent({ productObj }) {
     const { language } = useContext(LanguageContext)
+    const suffix = language == 'id' ? '' : '_en';
     const formater = new Intl.NumberFormat( language == 'id' ? 'id-ID' : 'en-EN', { style: 'currency', currency: language == 'id' ? 'IDR' : 'USD', minimumFractionDigits: 0, maximumFractionDigits: 2 })
-
-    console.log(productObj);
 
     return (
         <div className="product-description-wrapper">
@@ -53,16 +53,17 @@ export default function ProductDescriptionComponent({ productObj }) {
                         <h3 className='product-detail-title'>Product Detail</h3>
                         <p className='product-description-text'>{parse(productObj.description ? productObj.description : '')}</p>
                         <ul className='product-list-details'>
-                            { /* <li>roduct code: 1NE515_RDH0_F0F24</li> */ }
-                            { /* <li>Zipper closure</li> */ }
-                            { /* <li>Enameled metal triangle logo</li> */ }
-                            { /* <li>Woven tape handle</li> */ }
-                            { /* <li>Logo-print Re-Nylon lining</li> */}
-                            <li>Height: {productObj.height}cm</li>
-                            <li>Width: {productObj.width}cm</li>
-                            <li>Length: {productObj.length}cm</li>
-                            { /* <li>Imported</li> */}
+                            <li><strong>Brand</strong>: {productObj.brand}</li>
+                            <li><strong>Category</strong>: {productObj.category}</li>
+                            <li><strong>Weight</strong>: {StringUtil.numberingWithDotFormat(Math.ceil(productObj.weight))} gr</li>
+                            <li><strong>Height</strong>: {productObj.height}cm</li>
+                            <li><strong>Width</strong>: {productObj.width}cm</li>
+                            <li><strong>Length</strong>: {productObj.length}cm</li>
+                            <li><strong>Color</strong>: {productObj.color}</li>
+                            <li><strong>Condition</strong>: {productObj.condition}</li>
                         </ul>
+                        <h3 className='product-detail-title'>History</h3>
+                        <p className='product-description-text'>{parse(productObj['history'+suffix] ? productObj['history'+suffix] : '')}</p>
                     </div>
                     {/* <div className='product-materials'>
                         <h3 className='product-material-title'>Materials</h3>
