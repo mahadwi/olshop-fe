d81c8063279446b89a3770cda73db41e547436dd
MyFRA <tomyntapss@gmail.com>
2024-02-27 21:15:45 +1300

    feat: checkout change address show shipping modal

--- src/containers/shopping/checkout/ShoppingCheckout.jsx
+++ src/containers/shopping/checkout/ShoppingCheckout.jsx
@@ -193,6 +193,8 @@ export default function ShoppingCheckout() {
                     }
                 });
             // }
+        } else {
+            setModalChangeCourier(true);
         }
     }, [selectedCourier, selectedAddress]);
 
